b7daff4fdc2e602eafd82c7edd0c271c4a135a90
Tsubasa <tsubasanikander@gmail.com>
2022-10-27 12:43:20 +0200

    Move size settings to global

--- src/components/Main/styled.js
+++ src/components/Main/styled.js
@@ -1,12 +1,7 @@
 import styled from 'styled-components';
 
 export const Main = styled.main`
-    width: 100vw;
-    height: 100vh;
-    max-width: 100%;
     padding: 0 1rem;
-    
-    overflow-x: hidden;
     
     display: grid;
     place-content: center;
